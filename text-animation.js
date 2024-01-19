56598dfdb4f10d6ba4e517a5c84452d5665c1b65
wonderistweb <147849552+wonderistweb@users.noreply.github.com>
2024-01-19 14:22:25 -0800

    Update text-animation.js
    Add Line Animation

--- text-animation.js
+++ text-animation.js
@@ -82,3 +82,44 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // Avoid flash of unstyled content
   gsap.set("[text-split]", { opacity: 1 });
 });
+
+// Line Animation
+window.addEventListener("DOMContentLoaded", (event) => {
+  setTimeout(() => {
+    $("[js-line-animation]").each(function (index) {
+			gsap.set($(this), { autoAlpha: 1 });
+      let textEl = $(this);
+      let textContent = $(this).text();
+      let tl;
+
+      function splitText() {
+        new SplitType(textEl, { types: "lines", tagName: "span" });
+        textEl.find(".line").each(function (index) {
+          let lineContent = $(this).html();
+          $(this).html("");
+          $(this).append(`<span class="line-inner" style="display: block;">${lineContent}</span>`);
+        });
+        tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: textEl,
+            start: "top bottom",
+            end: "bottom bottom",
+            toggleActions: "none play none reset"
+          }
+        });
+        tl.fromTo(textEl.find(".line-inner"), { yPercent: 100 }, { yPercent: 0, duration: 0.3, stagger: { amount: 0.5, ease: "power1.out" } });
+      }
+      splitText();
+
+      let windowWidth = window.innerWidth;
+      window.addEventListener("resize", function () {
+        if (windowWidth !== window.innerWidth) {
+          windowWidth = window.innerWidth;
+          tl.kill();
+          textEl.text(textContent);
+          splitText();
+        }
+      });
+    });
+  }, 700);
+});
